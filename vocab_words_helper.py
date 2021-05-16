d33c370109f953a96db5460fbd995b435bee2b67
allison-hou <77060734+allison-hou@users.noreply.github.com>
2021-05-16 22:46:46 +0800

    Update vocab_words_helper.py

--- vocab_words_helper.py
+++ vocab_words_helper.py
@@ -1,10 +1,18 @@
-FILE_PATH = 'en.txt'
-OUTPUT_PATH = 'output.txt'
+import sys
 
-with open(FILE_PATH) as f:
-    lines = f.readlines()
-    lines = [line.strip() for line in lines]
-    lines.sort()
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    if len(args) != 2:
+        print('[USAGE] python vocab_words_helper.py <input_file_path> <output_file_path>')
+        
+       
+    FILE_PATH = args[1]
+    OUTPUT_PATH = args[2]
+    
+    with open(FILE_PATH) as f:
+        lines = f.readlines()
+        lines = [line.strip() for line in lines]
+        lines.sort()
 
-with open(OUTPUT_PATH, 'w') as f:
-    f.write('\n'.join(lines)) 
+    with open(OUTPUT_PATH, 'w') as f:
+        f.write('\n'.join(lines)) 
