626177646f5b85a2ecaec68225461e86344964d5
Allison Hou <77060734+allison-hou@users.noreply.github.com>
2022-04-10 09:42:39 +0800

    Update vocab_words_helper.py

--- vocab_words_helper.py
+++ vocab_words_helper.py
@@ -15,3 +15,28 @@ if __name__ == '__main__':
 
     with open(OUTPUT_PATH, 'w') as f:
         f.write('\n'.join(lines)) 
+
+import sys
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    if len(args) != 2:
+        print('[USAGE] python vocab_words_helper.py <input_file_path> <output_file_path> <question_output_path>')      
+       
+    FILE_PATH = args[1]
+    OUTPUT_PATH = args[2]
+    QUESTION_PATH = args[3]
+    
+    with open(FILE_PATH) as f:
+        lines = f.readlines()
+        lines = [line.strip() for line in lines]
+        lines.sort()
+
+    with open(OUTPUT_PATH, 'w') as f:
+        f.write('\n'.join(lines)) 
+    
+    with open(QUESTION_PATH, 'w') as f:
+        questions = []
+        for line in lines:
+            questions.append(line[0] + '_____' + line[-1])
+        f.write('\n'.join(lines))
